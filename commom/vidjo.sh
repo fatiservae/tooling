1bdc039b9d3a399e3d9c320d5fbe9cd3d4544484
jefferson <jefferson.science@gmail.com>
2025-02-16 15:10:40 -0300

    fix temporaio de uso de cookies

--- commom/vidjo.sh
+++ commom/vidjo.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-# By Jefferson T. @ 2022
-# Um baixador de vídeos dentro da capacidade de yt-dlp
-# Este script não adiciona capacidades a yt-dlp, apenas automatiza
-# e organiza seu funcionament.
-
 helper(){
 	echo 'Argumentos são:';
 	echo '	vidjo.sh:					baixa vídeo separado de áudio e toca juntos';
@@ -11,6 +6,10 @@ helper(){
 	echo '	vidjo.sh --merge:			baixa vídeo separado de áudio e mescla usando ffmpeg';
 	echo '	vidjo.sh --merge --del :	baixa vídeo separado de áudio, mescla e deleta arquivos separados';
 	exit
+}
+
+unite(){
+	ffmpeg -i "$1" -i "$2" -c:v copy -c:a copy "$3.mp4";
 }
 
 src="$(xclip -o -selection clipboard)";
@@ -27,19 +26,20 @@ case "${1-_is_unset_}" in
 	_is_unset_)
 		"$(yt-dlp -o "__audio$random" --format "ba" "$src")";
 		"$(yt-dlp -o "__video$random" --format "bv[height<1080]" "$src")";
-		ffmpeg -i "__video$random" -i "__audio$random" -c:v copy -c:a copy "$name.mp4";
+		unite "__video$random" "__audio$random" "$name";
 		rm "__audio$random" "__video$random";
-		#mpv  --cache=yes --stream-record="./$name.mp4" --audio-file="./__audio$name" "./__video$name";
-		#mpv --cache=yes --stream-record="./$name.mp4" --audio-file="./__audio$name" "./__video$name";
+		#mpv  --cache=yes --stream-record="./$name.mp4" --audio-file="./__audio$name" "./__video$random";
+		#mpv --cache=yes --stream-record="./$name.mp4" --audio-file="./__audio$name" "./__video$random";
 ;;
 	--simple)
-		"$(yt-dlp -o "__video$name" --format "[height<1080]" "$src")";
+		"$(yt-dlp -o "__video$random" --format "[height<1080]" "$src")";
 ;;
 # deprecado?
 	--merge)
 		"$(yt-dlp -o "__audio$name" --format "ba" "$src")";
-		"$(yt-dlp -o "__video$name" --format "bv[height<1080]" "$src")";
-		ffmpeg -i "./__video$name" -i "./__audio$name" -c:v copy -c:a aac "$name.mp4"
+		"$(yt-dlp -o "__video$random" --format "bv[height<1080]" "$src")";
+		unite "./__video$random" "./__audio$name" "$name";
+
 		case "$2" in 
 			*) 
 				echo 'help2!';
@@ -49,20 +49,26 @@ case "${1-_is_unset_}" in
 				rm "./__*";
 		;;
 		esac
+
 ;;
 	--yt-cookies)
 		##[ -z $(< /home/jefferson/.cookies) ] && echo 'Arquivo /home/jefferson/.cookies não encontrado' && exit
 		"$(yt-dlp --cookies "/home/jefferson/.cookies" -o "__audio$random" --format "ba" "$src")";
-		"$(yt-dlp --cookies "/home/jefferson/.cookies" -o "__video$random" --format "bv[height<1080]" "$src")";
-		ffmpeg -i "__video$random" -i "__audio$random" -c:v copy -c:a copy "$name.mp4";
+		"$(yt-dlp --cookies "/home/jefferson/.cookies" -o "__video$random" --format "bv[height<1080]" "$src")"
+		unite "./__video$random" "./__audio$random" "$name";
+		 
 ;;
-	*) 
+  --unite)
+    unite $2 $3 $4
+
+;;
+  *) 
 		helper;
 ;;
 esac
 #ext="$(yt-dlp --get-format $video)";
 #filename="$name.$ext";
-#mpv --cache=yes --stream-record="./$name.mp4" --audio-file="./__audio$name" "./__video$name";
+#mpv --cache=yes --stream-record="./$name.mp4" --audio-file="./__audio$name" "./__video$random";
 #yt-dlp -o  "__buff_video" -f "bv[height<=720]"+"ba" "$(xclip -o -selection clipboard)"
 #yt-dlp -o "__buff_audio" "$(xclip -o -selection clipboard)";
 #ffmpeg -i "./__buff_video" -i "./__buff_audio" -c:v copy -c:a aac video$RANDOM.mp4
